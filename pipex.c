33f0aaff465e9fddad39d065f7a91a8d923caae3
Jorge Vacarisas bofill <jvacaris@c1r16s5.42madrid.com>
2021-10-03 20:48:02 +0200

    day X

--- pipex.c
+++ pipex.c
@@ -3,17 +3,24 @@
 void	child_one(char *argv, char **envp, int fdin, int fd[2])
 {
 	close(fd[0]);
-	dup2(fdin, STDIN_FILENO); 
-	close(fd[1]);
+	dup2(fdin, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+//	close(fd[1]);
+	close(fdin);
 	execve("/bin/cat", ft_split(argv, ' '), envp);
 }
 
 void	child_two(char *argv, char **envp, int fdout, int fd[2])
 {
-	close(fd[1]);
-	dup2(fdout, STDOUT_FILENO); 
+//	close(fd[1]);
+	char *test = malloc(25);
+	test[9] = 24;
+	read(fd[0], test, 24);
+	printf("\n\n>%s<\n\n", test);
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fdout, STDOUT_FILENO);
 	close(fd[0]);
-	execve("/bin/cat", ft_split(argv, ' '), envp);
+	execve("/bin/wc", ft_split(argv, ' '), envp);
 }
 
 void	pipex(char **argv, char **envp, int fdin, int fdout)
@@ -25,13 +32,13 @@ void	pipex(char **argv, char **envp, int fdin, int fdout)
 	relative = fork();
 	if (!relative)
 		child_one(argv[2], envp, fdin, fd);
-		else
-		{
-			relative = fork();
-			if (!relative)
-				child_two(argv[3], envp, fdin, fd);
-		}
-
+	else
+	{
+		relative = fork();
+		if (!relative)
+			child_two(argv[3], envp, fdout, fd);
+	}
+	wait(&relative);
 }
 
 int main(int argc, char **argv, char **envp)
@@ -39,8 +46,10 @@ int main(int argc, char **argv, char **envp)
 	int	fdin;
 	int	fdout;
 
+	if (argc != 5)
+		return (0);
 	fdin = open(argv[1], 0);
-	fdout = open(argv[4], 0);
+	fdout = open(argv[4], 1);
 	pipex(argv, envp, fdin, fdout);
 
 	return (0);
